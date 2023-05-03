0183d0e8ca4acaa3f2f0c18ecd0b04cdcd645f8c
Mhammed Doukkali <mhammed.doukkali@outlook.com>
2023-05-03 17:52:25 -0400

    implement Controller

--- core_java/twitter/src/main/java/ca/jrvs/apps/twitter/controller/TwitterController.java
+++ core_java/twitter/src/main/java/ca/jrvs/apps/twitter/controller/TwitterController.java
@@ -2,36 +2,82 @@ package ca.jrvs.apps.twitter.controller;
 
 import ca.jrvs.apps.twitter.dao.CrdDao;
 import ca.jrvs.apps.twitter.model.Tweet;
-import ca.jrvs.apps.twitter.service.TwitterService;
+import ca.jrvs.apps.twitter.service.Service;
+import ca.jrvs.apps.twitter.util.TweetUtil;
+
+import org.springframework.util.StringUtils;
 
 import java.util.List;
 
 public class TwitterController implements Controller {
 
-    private TwitterService tweetService;
-    private CrdDao dao;
+    private static final String COORD_SEP = ":";
+    private static final String COMMA = ",";
+    private Service service;
 
-    public TwitterController(TwitterService tweetService) {
-        this.tweetService = tweetService;
+    public TwitterController(Service service) {
+        this.service = service;
     }
     @Override
     public Tweet postTweet(String[] args) {
         // this methods have to be revised
-        if(args == null || args.length == 0) {
-            throw new IllegalArgumentException("Invalid arguments");
+        if (args.length != 3) {
+            throw new IllegalArgumentException(
+                    "USAGE: TwitterCLIApp post \"tweet_text\" \"latitude:longitude\"");
         }
-        String text = args[0];
+        String tweet_text = args[1];
+        String coord = args[2];
+        String[] coordArray = coord.split(COORD_SEP);
+        Double lon = null;
+        Double lat = null;
+        if (coordArray.length != 2 || StringUtils.isEmpty(tweet_text)) {
+            throw new IllegalArgumentException(
+                    "Invalid location format\nUSAGE: TwitterCLIApp post \"tweet_text\" \"latitude:longitude\""
+            );
+        }
 
-        return (Tweet) dao.create(text);
+            try {
+                lat = Double.parseDouble(coordArray[0]);
+                lon = Double.parseDouble(coordArray[1]);
+            } catch (Exception e) {
+                throw new IllegalArgumentException(
+                        "Invalid location format\nUSAGE: TwitterCLIApp post \"tweet_text\" \"latitude:longitude\""
+                        , e);
+            }
+
+        Tweet newTweet = TweetUtil.builtTweet(tweet_text, lon, lat);
+        return service.postTweet(newTweet);
     }
 
     @Override
     public Tweet showTweet(String[] args) {
-        return null;
+        if(args.length != 2 && args.length != 3) {
+            throw new IllegalArgumentException(
+                    "USAGE: TwitterCLIApp show \"tweet_id\" \"fields1, fields2, fields3...\""
+            );
+        }
+        String id = args[1];
+        String[] fields = null;
+        if(args.length == 3) {
+            if(StringUtils.isEmpty(args[2])) {
+                throw new IllegalArgumentException(
+                        "USAGE: TwitterCLIApp show \"tweet_id\" \"fields1, fields2, fields3...\""
+                );
+            }
+            fields = args[2].split(COMMA);
+        }
+        return service.showTweet(id, fields);
     }
 
     @Override
     public List<Tweet> deleteTweet(String[] args) {
-        return null;
+        if (args.length != 2)
+            throw new IllegalArgumentException(
+                    "USAGE: TwitterCLIApp delete \"[id1, id2,...]\""
+            );
+            String[] ids = args[1].split(COMMA);
+
+        return service.deleteTweets(ids);
+
     }
 }
